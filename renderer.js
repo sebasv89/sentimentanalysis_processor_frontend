b2d9818535b9e1e2c086bd33e854cf77dd8234a8
Sebastian Velez <sebasv89@gmail.com>
2016-12-14 23:14:15 -0500

    first version

---
+++ renderer.js
@@ -0,0 +1,20 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+jQuery(function(){
+
+     jQuery.get('http://localhost:8080/rest/people', function(response){
+         if (response === null || response.length <= 0){
+             $("#personName").append('<option></option>');
+         }
+
+        $.each(response, function (i, item) {
+            $('#personName').append($('<option>', { 
+                value: item,
+                text : item 
+            }));
+        });
+        
+    });
+
+});
