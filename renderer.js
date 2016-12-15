4b6751d9e30d7c706c93fbd32084fd54b07f10be
Sebastian Velez <sebasv89@gmail.com>
2016-12-14 23:31:13 -0500

    first interactions with the backend

--- renderer.js
+++ renderer.js
@@ -6,6 +6,7 @@ jQuery(function(){
      jQuery.get('http://localhost:8080/rest/people', function(response){
          if (response === null || response.length <= 0){
              $("#personName").append('<option></option>');
+             return;
          }
 
         $.each(response, function (i, item) {
